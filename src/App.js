6d8ec9f4238dfbe9cc2d8baa4113b2c3e4364f01
ArturLabenskyy <dodik.dod@hotmail.com>
2023-03-28 18:03:04 +0300

    added card and post components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './styles/App.css';
+import "./styles/App.css";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return <div className="App"></div>;
 }
 
 export default App;
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,13 +1,45 @@
-import React from 'react'
-import Card from '../Card/Card'
+import React from "react";
+import Card from "../Card/Card";
+import "./Post.css";
 
-const Post = () => {
-  return (
-    <div>
-        post content
-        <Card/>
-    </div>
-  )
-}
+const Post = ({ arrayOfPosts }) => {
+    const arr = [
+        {
+            title: "Post 1",
+            imageLink: "https://i.redd.it/u8b2pgrblij71.png",
+            postLink:
+                "https://www.reddit.com/r/RedditforBusiness/comments/113yfmq/marketing_learnings_from_super_bowl_lvii/",
+        },
+        {
+            title: "Post 2",
+            imageLink: null,
+            postLink:
+                "https://www.reddit.com/r/news/comments/123ksgt/largest_strike_in_decades_brings_germany_to_a/",
+        },
+        {
+            title: "Post 3",
+            imageLink: null,
+            postLink:
+                "https://www.reddit.com/r/comics/comments/123vwrk/wedding_mirrors_oc/",
+        },
+        {
+            title: "Post 4",
+            imageLink: null,
+            postLink:
+                "https://www.reddit.com/r/RedditforBusiness/comments/113yfmq/marketing_learnings_from_super_bowl_lvii/",
+        },
+    ];
+    return (
+        <div className="post">
+            {(!arrayOfPosts ? arr : arrayOfPosts).map((el) => (
+                <Card
+                    title={el.title}
+                    postLink={el.postLink}
+                    imageLink={el.imageLink}
+                />
+            ))}
+        </div>
+    );
+};
 
-export default Post
+export default Post;
